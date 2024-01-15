9491023e83cc54578260314303410c0dbb90ea0a
Jaime Albuquerque <jaime.albq@gmail.com>
2024-01-15 09:59:46 -0300

    Original

--- src/main.ino
+++ src/main.ino
@@ -1,21 +1,17 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/queue.h"
-
 #define BTN 5
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Pushbutton Counter");
-  pinMode(PIN_BUTTON, INPUT_PULLUP);
+  pinMode(BTN, INPUT_PULLUP);
 }
 
 void loop()
 {
   static int count = 0;
   static int lastState = HIGH;
-  int state = digitalRead(PIN_BUTTON);
+  int state = digitalRead(BTN);
   if (state != lastState)
   {
     if (state == LOW)
@@ -27,5 +23,5 @@ void loop()
     }
     lastState = state;
   }
-  vTaskDelay(10 / portTICK_PERIOD_MS);
+  delay(10);
 }
